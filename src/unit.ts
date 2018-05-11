f9bac187c0d5dc3342c2406faffde215b06a4ef4
Jonah Scheinerman <jonah@scheinerman.net>
2018-05-11 10:54:35 -0400

    Unit class

--- src/unit.ts
+++ src/unit.ts
@@ -1,10 +1,10 @@
-import { base, divide, square } from "./units/valueArithmetic";
+import { Unit } from "./units/unit";
 
-const meter = base("length");
-const second = base("time");
+const meter = Unit.basis("length");
+const second = Unit.basis("time");
 
-const mps = divide(meter, second);
-export type Velocity = typeof mps;
+const mps = meter.per(second);
+export type Velocity = typeof mps.vector;
 
-const mps2 = divide(meter, square(second));
-export type Acceleration = typeof mps2;
+const mps2 = meter.per(second.squared());
+export type Acceleration = typeof mps2.vector;
---
+++ src/units/unit.ts
@@ -0,0 +1,40 @@
+import { MinimalDimensionVector, Dimension } from "./common";
+import { MultiplyUnits, DivideUnits, ExponentiateUnit } from "./typeArithmetic";
+import { multiply, divide, exponentiate, base } from "./vectorArithmetic";
+import { Exponent } from "../exponents/common";
+
+export class Unit<Vector extends MinimalDimensionVector> {
+    constructor(public readonly vector: Vector) {}
+
+    public static scalar() {
+        return new Unit({});
+    }
+
+    public static basis<Dim extends Dimension>(dimension: Dim) {
+        return new Unit(base(dimension));
+    }
+
+    public times<Other extends MinimalDimensionVector>(other: Unit<Other>): Unit<MultiplyUnits<Vector, Other>> {
+        return new Unit(multiply(this.vector, other.vector));
+    }
+
+    public per<Other extends MinimalDimensionVector>(other: Unit<Other>): Unit<DivideUnits<Vector, Other>> {
+        return new Unit(divide(this.vector, other.vector));
+    }
+
+    public toThe<Power extends Exclude<Exponent, 0 | 1>>(power: Power): Unit<ExponentiateUnit<Vector, Power>> {
+        return new Unit(exponentiate(this.vector, power));
+    }
+
+    public inverse() {
+        return this.toThe(-1);
+    }
+
+    public squared() {
+        return this.toThe(2);
+    }
+
+    public cubed() {
+        return this.toThe(3);
+    }
+}
