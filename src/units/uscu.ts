f5eebcf1bb0fa3d1d6296cbf85280a6f7c2fc36a
Jonah Scheinerman <jonah@scheinerman.net>
2018-05-19 10:23:12 -0400

    Pluralize US customary units

--- src/units/uscu.ts
+++ src/units/uscu.ts
@@ -5,35 +5,35 @@ import { micro } from "./metric";
 import { liters } from "./other";
 
 // Length
-export const point = Measure.of(352.778, micro(meters), "p");
-export const pica = Measure.of(12, point, "pica");
+export const points = Measure.of(352.778, micro(meters), "p");
+export const picas = Measure.of(12, points, "pica");
 
 // Liquid volume
-export const minim = Measure.of(61.611_519_921_875, micro(liters), "min");
-export const fluidDram = Measure.of(60, minim, "fl dr");
-export const teaspoon = Measure.of(80, minim, "tsp");
-export const tablespoon = Measure.of(3, teaspoon, "Tbsp");
-export const fluidOunce = Measure.of(2, tablespoon, "fl oz");
-export const shot = Measure.of(3, tablespoon, "jig");
-export const gill = Measure.of(4, fluidOunce, "gi");
-export const cup = Measure.of(2, gill, "cp");
-export const pint = Measure.of(2, cup, "pt");
-export const quart = Measure.of(2, pint, "qt");
-export const gallon = Measure.of(4, quart, "gal");
-export const barrel = Measure.of(31.5, gallon, "liq bbl");
-export const oilBarrel = Measure.of(42, gallon, "bbl");
-export const hogshead = Measure.of(63, gallon, "hogshead");
+export const minims = Measure.of(61.611_519_921_875, micro(liters), "min");
+export const fluidDrams = Measure.of(60, minims, "fl dr");
+export const teaspoons = Measure.of(80, minims, "tsp");
+export const tablespoons = Measure.of(3, teaspoons, "Tbsp");
+export const fluidOunces = Measure.of(2, tablespoons, "fl oz");
+export const shots = Measure.of(3, tablespoons, "jig");
+export const gills = Measure.of(4, fluidOunces, "gi");
+export const cups = Measure.of(2, gills, "cp");
+export const pints = Measure.of(2, cups, "pt");
+export const quarts = Measure.of(2, pints, "qt");
+export const gallons = Measure.of(4, quarts, "gal");
+export const barrels = Measure.of(31.5, gallons, "liq bbl");
+export const oilBarrels = Measure.of(42, gallons, "bbl");
+export const hogsheads = Measure.of(63, gallons, "hogshead");
 
 // Dry volume
-export const dryPint = Measure.of(0.550_610_471_3575, liters, "dry pt");
-export const dryQuart = Measure.of(2, dryPint, "dry qt");
-export const dryGallon = Measure.of(4, dryQuart, "dry gal");
-export const peck = Measure.of(2, dryGallon, "pk");
-export const bushel = Measure.of(4, peck, "bu");
-export const dryBarrel = Measure.of(7056, cubic(inches), "dry bbl");
+export const dryPints = Measure.of(0.550_610_471_3575, liters, "dry pt");
+export const dryQuarts = Measure.of(2, dryPints, "dry qt");
+export const dryGallons = Measure.of(4, dryQuarts, "dry gal");
+export const pecks = Measure.of(2, dryGallons, "pk");
+export const bushels = Measure.of(4, pecks, "bu");
+export const dryBarrels = Measure.of(7056, cubic(inches), "dry bbl");
 
 // Mass
-export const dram = Measure.of(1.771_845_195_3125, grams, "dr");
-export const pennyweight = Measure.of(24, grains, "dwt");
-export const hundredweight = Measure.of(100, pounds, "cwd");
-export const ton = Measure.of(2000, pounds, "ton");
+export const drams = Measure.of(1.771_845_195_3125, grams, "dr");
+export const pennyweights = Measure.of(24, grains, "dwt");
+export const hundredweights = Measure.of(100, pounds, "cwd");
+export const tons = Measure.of(2000, pounds, "ton");
