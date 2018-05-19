d047f9a46a0a277f4c97e0e6cb4366688c6be659
Jonah Scheinerman <jonah@scheinerman.net>
2018-05-19 09:50:21 -0400

    Measure formatting (#11)
    * Measure formatting
    
    * Fix lint

--- src/measure/__test__/measureTests.ts
+++ src/measure/__test__/measureTests.ts
@@ -1,10 +1,17 @@
 import { Measure } from "../measure";
+import { Unit } from "../units";
 
 describe("Measures", () => {
-    const meter = Measure.dimension("length");
-    const second = Measure.dimension("time");
+    const meter = Measure.dimension("m");
+    const second = Measure.dimension("s");
     const mps = meter.per(second);
     const mps2 = mps.per(second);
+
+    class TestMeasure<U extends Unit> extends Measure<Unit> {
+        constructor(value: number, unit: U, symbol?: string | undefined) {
+            super(value, unit, symbol);
+        }
+    }
 
     describe("construction", () => {
         it("should construct from a number of and a unit", () => {
@@ -80,7 +87,7 @@ describe("Measures", () => {
         });
     });
 
-    describe("Comparison", () => {
+    describe("comparison", () => {
         const zero = Measure.of(0, meter);
         const five = Measure.of(5, meter);
         const ten = Measure.of(10, meter);
@@ -119,6 +126,82 @@ describe("Measures", () => {
             expect(five.isGreaterThan(zero)).toBe(true);
             expect(five.isGreaterThan(five)).toBe(false);
             expect(five.isGreaterThan(ten)).toBe(false);
+        });
+    });
+
+    describe("symbols", () => {
+        it("should assign a symbol via .of()", () => {
+            expect(Measure.of(1000, meter, "km").getSymbol()).toBe("km");
+        });
+
+        it("should copy assign a symbol via .withSymbol()", () => {
+            const original = Measure.of(1000, meter);
+            const result = original.withSymbol("km");
+            expect(result).not.toBe(original);
+            expect(original.getSymbol()).toBeUndefined();
+            expect(result.getSymbol()).toBe("km");
+        });
+
+        it("should not pass along symbols through operations", () => {
+            const km = Measure.of(1000, meter.squared()).withSymbol("km2");
+            const dm = Measure.of(10, meter.squared()).withSymbol("dm2");
+            expect(km.normalized().getSymbol()).toBeUndefined();
+            expect(km.negate().getSymbol()).toBeUndefined();
+            expect(km.squared().getSymbol()).toBeUndefined();
+            expect(km.sqrt().getSymbol()).toBeUndefined();
+            expect(km.inverse().getSymbol()).toBeUndefined();
+            expect(km.plus(dm).getSymbol()).toBeUndefined();
+            expect(km.minus(dm).getSymbol()).toBeUndefined();
+            expect(km.times(dm).getSymbol()).toBeUndefined();
+            expect(km.over(dm).getSymbol()).toBeUndefined();
+        });
+    });
+
+    describe("formatting", () => {
+        it("should format scalar units", () => {
+            expect(Measure.scalar(10).toString()).toBe("10 scalar");
+            expect(new TestMeasure(10, { x: 0, y: undefined }).toString()).toBe("10 scalar");
+        });
+
+        it("should format base units", () => {
+            expect(meter.toString()).toBe("1 m");
+            expect(Measure.of(5.3, meter).toString()).toBe("5.3 m");
+        });
+
+        it("should format complex units", () => {
+            expect(Measure.of(5, meter.squared()).toString()).toBe("5 m^2");
+            expect(Measure.of(5, second.inverse()).toString()).toBe("5 s^-1");
+            expect(Measure.of(5, meter.times(second)).toString()).toBe("5 m * s");
+            expect(Measure.of(5, meter.over(second)).toString()).toBe("5 m * s^-1");
+            expect(Measure.of(5, meter.cubed().over(second)).toString()).toBe("5 m^3 * s^-1");
+            expect(Measure.of(5, meter.cubed().over(second.squared())).toString()).toBe("5 m^3 * s^-2");
+        });
+
+        it("should not format using symbol even if present", () => {
+            expect(
+                Measure.of(5, meter.squared())
+                    .withSymbol("m2")
+                    .toString(),
+            ).toBe("5 m^2");
+            expect(
+                Measure.scalar(0)
+                    .withSymbol("rad")
+                    .toString(),
+            ).toBe("0 scalar");
+        });
+
+        it("should skip formatting explicitly 0 and undefined dimension", () => {
+            expect(new TestMeasure(10, { x: 0, y: undefined, z: 2 }).toString()).toBe("10 z^2");
+        });
+
+        it("should format measures as other measures with symbols", () => {
+            const glorbs = Measure.of(100, meter, "glb");
+            expect(Measure.of(1000, meter).in(glorbs)).toBe("10 glb");
+        });
+
+        it("should use normal formatting if the other measure has no symbol", () => {
+            const glorbs = Measure.of(100, meter);
+            expect(Measure.of(1000, meter).in(glorbs)).toBe("1000 m");
         });
     });
 });
