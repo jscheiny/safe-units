76bb33eab72ab5b1daeec76896561b08d8bdaa27
Jonah Scheinerman <jonah@scheinerman.net>
2018-05-21 15:06:11 -0400

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import * as Unit from "./unit";
 
 export * from "./measure";
 export * from "./quantity";
+export * from "./unit";
 export { Unit };
--- src/quantity/index.ts
+++ src/quantity/index.ts
@@ -1,2 +1,3 @@
 import * as Quantity from "./quantities";
+export * from "./quantities";
 export { Quantity };
