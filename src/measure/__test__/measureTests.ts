7a3470622bb7803b027462d1bec14a560ce9daba
Jonah Scheinerman <jonah@scheinerman.net>
2018-05-30 01:20:15 -0400

    Pull out math function into separate file

--- src/measure/__test__/measureTests.ts
+++ src/measure/__test__/measureTests.ts
@@ -1,18 +1,13 @@
-import { meters, seconds } from "../../unit";
+import { meters, pi, radians, seconds } from "../../unit";
+import { degrees } from "../../unit/angle";
+import * as SafeMath from "../math";
 import { cubic, Measure, square } from "../measure";
-import { Unit } from "../units";
 
 describe("Measures", () => {
     const meter = Measure.dimension("L");
     const second = Measure.dimension("T");
     const mps = meter.per(second);
     const mps2 = mps.per(second);
-
-    class TestMeasure<U extends Unit> extends Measure<Unit> {
-        constructor(value: number, unit: U, symbol?: string | undefined) {
-            super(value, unit, symbol);
-        }
-    }
 
     describe("dimension", () => {
         it("should create dimensions with value 1", () => {
@@ -170,7 +165,7 @@ describe("Measures", () => {
     describe("formatting", () => {
         it("should format dimensionless units", () => {
             expect(Measure.dimensionless(10).toString()).toBe("10");
-            expect(new TestMeasure(10, { x: 0, y: undefined }).toString()).toBe("10");
+            expect(Measure.unsafeConstruct(10, { x: 0, y: undefined }).toString()).toBe("10");
         });
 
         it("should format base units", () => {
@@ -211,7 +206,7 @@ describe("Measures", () => {
         });
 
         it("should skip formatting explicitly 0 and undefined dimension", () => {
-            expect(new TestMeasure(10, { x: 0, y: undefined, z: 2 }).toString()).toBe("10 z^2");
+            expect(Measure.unsafeConstruct(10, { x: 0, y: undefined, z: 2 }).toString()).toBe("10 z^2");
         });
 
         it("should format measures as other measures with symbols", () => {
@@ -237,64 +232,76 @@ describe("Measures", () => {
         const mps = meters.per(seconds);
 
         it("arithmetic", () => {
-            expect(Measure.add(Measure.of(5, mps), Measure.of(-5, mps))).toEqual(Measure.of(0, mps));
-            expect(Measure.subtract(Measure.of(5, mps), Measure.of(-5, mps))).toEqual(Measure.of(10, mps));
-            expect(Measure.multiply(Measure.of(5, mps), Measure.of(10, seconds))).toEqual(Measure.of(50, meters));
-            expect(Measure.divide(Measure.of(50, meters), Measure.of(10, seconds))).toEqual(Measure.of(5, mps));
+            expect(SafeMath.add(Measure.of(5, mps), Measure.of(-5, mps))).toEqual(Measure.of(0, mps));
+            expect(SafeMath.subtract(Measure.of(5, mps), Measure.of(-5, mps))).toEqual(Measure.of(10, mps));
+            expect(SafeMath.multiply(Measure.of(5, mps), Measure.of(10, seconds))).toEqual(Measure.of(50, meters));
+            expect(SafeMath.divide(Measure.of(50, meters), Measure.of(10, seconds))).toEqual(Measure.of(5, mps));
+        });
+
+        it("trig", () => {
+            const zeroRadians = Measure.of(0, radians);
+            const zero = Measure.dimensionless(0);
+            expect(SafeMath.cos(zeroRadians)).toEqual(Measure.dimensionless(1));
+            expect(SafeMath.sin(zeroRadians)).toEqual(zero);
+            expect(SafeMath.tan(zeroRadians)).toEqual(zero);
+            expect(SafeMath.acos(zero)).toEqual(Measure.of(0.5, pi));
+            expect(SafeMath.asin(zero)).toEqual(zeroRadians);
+            expect(SafeMath.atan(zero)).toEqual(zeroRadians);
+            expect(SafeMath.atan2(Measure.of(5, meters), Measure.of(5, meters))).toEqual(Measure.of(45, degrees));
         });
 
         it("abs", () => {
-            expect(Measure.abs(Measure.of(-10, mps))).toEqual(Measure.of(10, mps));
+            expect(SafeMath.abs(Measure.of(-10, mps))).toEqual(Measure.of(10, mps));
         });
 
         it("cbrt", () => {
-            expect(Measure.cbrt(Measure.of(64, seconds.cubed()))).toEqual(Measure.of(4, seconds));
+            expect(SafeMath.cbrt(Measure.of(64, seconds.cubed()))).toEqual(Measure.of(4, seconds));
         });
 
         it("ceil", () => {
-            expect(Measure.ceil(Measure.of(3.4, mps))).toEqual(Measure.of(4, mps));
+            expect(SafeMath.ceil(Measure.of(3.4, mps))).toEqual(Measure.of(4, mps));
         });
 
         it("floor", () => {
-            expect(Measure.floor(Measure.of(7.8, mps))).toEqual(Measure.of(7, mps));
+            expect(SafeMath.floor(Measure.of(7.8, mps))).toEqual(Measure.of(7, mps));
         });
 
         it("hypot", () => {
-            expect(Measure.hypot(Measure.of(3, meters), Measure.of(4, meters))).toEqual(Measure.of(5, meters));
+            expect(SafeMath.hypot(Measure.of(3, meters), Measure.of(4, meters))).toEqual(Measure.of(5, meters));
         });
 
         it("max", () => {
-            expect(Measure.max(Measure.of(10, mps), Measure.of(5, mps), Measure.of(15, mps))).toEqual(
+            expect(SafeMath.max(Measure.of(10, mps), Measure.of(5, mps), Measure.of(15, mps))).toEqual(
                 Measure.of(15, mps),
             );
         });
 
         it("min", () => {
-            expect(Measure.min(Measure.of(10, mps), Measure.of(5, mps), Measure.of(15, mps))).toEqual(
+            expect(SafeMath.min(Measure.of(10, mps), Measure.of(5, mps), Measure.of(15, mps))).toEqual(
                 Measure.of(5, mps),
             );
         });
 
         it("pow", () => {
-            expect(Measure.pow(Measure.of(10, meters), 3)).toEqual(Measure.of(1000, meters.cubed()));
+            expect(SafeMath.pow(Measure.of(10, meters), 3)).toEqual(Measure.of(1000, meters.cubed()));
         });
 
         it("round", () => {
-            expect(Measure.round(Measure.of(7.8, mps))).toEqual(Measure.of(8, mps));
+            expect(SafeMath.round(Measure.of(7.8, mps))).toEqual(Measure.of(8, mps));
         });
 
         it("sqrt", () => {
-            expect(Measure.sqrt(Measure.of(25, meters.squared()))).toEqual(Measure.of(5, meters));
+            expect(SafeMath.sqrt(Measure.of(25, meters.squared()))).toEqual(Measure.of(5, meters));
         });
 
         it("sum", () => {
-            expect(Measure.sum(Measure.of(10, mps), Measure.of(5, mps), Measure.of(15, mps))).toEqual(
+            expect(SafeMath.sum(Measure.of(10, mps), Measure.of(5, mps), Measure.of(15, mps))).toEqual(
                 Measure.of(30, mps),
             );
         });
 
         it("trunc", () => {
-            expect(Measure.trunc(Measure.of(-7.8, mps))).toEqual(Measure.of(-7, mps));
+            expect(SafeMath.trunc(Measure.of(-7.8, mps))).toEqual(Measure.of(-7, mps));
         });
     });
 });
---
+++ src/measure/math.ts
@@ -0,0 +1,96 @@
+import { Exponent } from "../exponent";
+import { Dimensionless, Length, PlaneAngle } from "../quantity";
+import { radians } from "../unit";
+import { Measure } from "./measure";
+import { DivideUnits, ExponentiateUnit, MultiplyUnits, NthRootableUnit, NthRootUnit } from "./types";
+import { nthRootUnit, Unit } from "./units";
+
+export const abs = wrapUnary(Math.abs);
+export const acos = wrapInverseTrig(Math.acos);
+export const asin = wrapInverseTrig(Math.asin);
+export const atan = wrapInverseTrig(Math.atan);
+export const ceil = wrapUnary(Math.ceil);
+export const cos = wrapTrig(Math.cos);
+export const floor = wrapUnary(Math.floor);
+export const fround = wrapUnary(Math.fround);
+export const hypot = warpNary(Math.hypot);
+export const max = warpNary(Math.max);
+export const min = warpNary(Math.min);
+export const round = wrapUnary(Math.round);
+export const sin = wrapTrig(Math.sin);
+export const tan = wrapTrig(Math.tan);
+export const trunc = wrapUnary(Math.trunc);
+
+export function atan2(y: Length, x: Length): PlaneAngle {
+    return Measure.of(Math.atan2(y.value, x.value), radians);
+}
+
+export function pow<U extends Unit, Y extends Exponent>(x: Measure<U>, y: Y): Measure<ExponentiateUnit<U, Y>> {
+    return x.toThe(y);
+}
+
+export function sqrt<U extends NthRootableUnit<2>>(x: Measure<U>): Measure<NthRootUnit<U, 2>> {
+    return Measure.unsafeConstruct(Math.sqrt(x.value), nthRootUnit(x.getUnit(), 2));
+}
+
+export function cbrt<U extends NthRootableUnit<3>>(x: Measure<U>): Measure<NthRootUnit<U, 3>> {
+    return Measure.unsafeConstruct(Math.cbrt(x.value), nthRootUnit(x.getUnit(), 3));
+}
+
+export function add<U extends Unit>(left: Measure<U>, right: Measure<U>): Measure<U> {
+    return left.plus(right);
+}
+
+export function subtract<U extends Unit>(left: Measure<U>, right: Measure<U>): Measure<U> {
+    return left.minus(right);
+}
+
+export function multiply<L extends Unit, R extends Unit>(
+    left: Measure<L>,
+    right: Measure<R>,
+): Measure<MultiplyUnits<L, R>> {
+    return left.times(right);
+}
+
+export function divide<L extends Unit, R extends Unit>(
+    left: Measure<L>,
+    right: Measure<R>,
+): Measure<DivideUnits<L, R>> {
+    return left.over(right);
+}
+
+export function sum<U extends Unit>(first: Measure<U>, ...rest: Array<Measure<U>>): Measure<U> {
+    let result = first;
+    for (const measure of rest) {
+        result = result.plus(measure);
+    }
+    return result;
+}
+
+function wrapUnary(f: (x: number) => number) {
+    return <U extends Unit>(x: Measure<U>): Measure<U> => {
+        return Measure.of(f(x.value), x.normalized());
+    };
+}
+
+function wrapTrig(f: (x: number) => number) {
+    return (angle: PlaneAngle): Dimensionless => {
+        return Measure.dimensionless(f(angle.value));
+    };
+}
+
+function wrapInverseTrig(f: (x: number) => number) {
+    return (angle: Dimensionless): PlaneAngle => {
+        return Measure.of(f(angle.value), radians);
+    };
+}
+
+function warpNary(f: (...x: number[]) => number) {
+    return <U extends Unit>(first: Measure<U>, ...rest: Array<Measure<U>>): Measure<U> => {
+        return Measure.of(f(...values(first, ...rest)), first.normalized());
+    };
+}
+
+function values<U extends Unit>(...measures: Array<Measure<U>>): number[] {
+    return measures.map(measure => measure.value);
+}
