d3181287a511799b8b30b8f1709d843b27420310
Jonah Scheinerman <jonah@scheinerman.net>
2018-08-09 16:45:18 -0400

    Refactor arithmetic spec

--- src/measure/__test__/arithmeticSpec.ts
+++ src/measure/__test__/arithmeticSpec.ts
@@ -1,44 +1,52 @@
 import { IsArithmeticError } from "../../exponent";
 import { DivideUnits, ExponentiateUnit, MultiplyUnits, NthRootableUnit, NthRootUnit } from "../types";
-import {} from "./testUtils";
+import { IsSame } from "./testUtils";
 
-type SelfMultiplicationWorks = { a: 4 } extends MultiplyUnits<{ a: 2 }, { a: 2 }> ? true : never;
+// MultiplyUnits
+
+type SelfMultiplicationWorks = IsSame<{ a: 4 }, MultiplyUnits<{ a: 2 }, { a: 2 }>>;
 const SelfMultiplicationWorks: SelfMultiplicationWorks = true;
 
-type MultiplySeveralDimensionsWorks = { b: 1; c: 1 } extends MultiplyUnits<{ a: 2; b: -1 }, { a: -2; b: 2; c: 1 }>
-    ? true
-    : never;
+type MultiplySeveralDimensionsWorks = IsSame<{ b: 1; c: 1 }, MultiplyUnits<{ a: 2; b: -1 }, { a: -2; b: 2; c: 1 }>>;
 const MultiplySeveralDimensionsWorks: MultiplySeveralDimensionsWorks = true;
 
 type MultiplicationPropagatesErrors = IsArithmeticError<MultiplyUnits<{ a: -5; b: 2 }, { a: -5; b: 1 }>>;
 const MultiplicationPropagatesErrors: MultiplicationPropagatesErrors = true;
 
-type DivisionWorks = { b: 1; c: 1 } extends DivideUnits<{ a: 2; b: -1 }, { a: 2; b: -2; c: -1 }> ? true : never;
+// DivideUnits
+
+type DivisionWorks = IsSame<{ b: 1; c: 1 }, DivideUnits<{ a: 2; b: -1 }, { a: 2; b: -2; c: -1 }>>;
 const DivisionWorks: DivisionWorks = true;
 
 type DivisionPropagatesErrors = IsArithmeticError<DivideUnits<{ a: -5; b: 1 }, { a: 5; b: 2 }>>;
 const DivisionPropagatesErrors: DivisionPropagatesErrors = true;
 
-type RaisingToTheZeroWorks = {} extends ExponentiateUnit<{ a: 2; b: -1 }, 0> ? true : never;
+// ExponentiateUnit
+
+type RaisingToTheZeroWorks = IsSame<{}, ExponentiateUnit<{ a: 2; b: -1 }, 0>>;
 const RaisingToTheZeroWorks: RaisingToTheZeroWorks = true;
 
-type RaisingToTheOneWorks = { a: 2; b: 3 } extends ExponentiateUnit<{ a: 2; b: 3 }, 1> ? true : never;
+type RaisingToTheOneWorks = IsSame<{ a: 2; b: 3 }, ExponentiateUnit<{ a: 2; b: 3 }, 1>>;
 const RaisingToTheOneWorks: RaisingToTheOneWorks = true;
 
-type SquaringWorks = { a: 4; b: -2 } extends ExponentiateUnit<{ a: 2; b: -1 }, 2> ? true : never;
+type SquaringWorks = IsSame<{ a: 4; b: -2 }, ExponentiateUnit<{ a: 2; b: -1 }, 2>>;
 const SquaringWorks: SquaringWorks = true;
 
-type CubingWorks = { a: 3 } extends ExponentiateUnit<{ a: 1 }, 3> ? true : never;
+type CubingWorks = IsSame<{ a: 3 }, ExponentiateUnit<{ a: 1 }, 3>>;
 const CubingWorks: CubingWorks = true;
 
 type ExponentiationPropagatesErrors = IsArithmeticError<ExponentiateUnit<{ a: -5; b: 1 }, -2>>;
 const ExponentiationPropagatesErrors: ExponentiationPropagatesErrors = true;
 
-type SquareRootingWorks = { a: 2; b: -1 } extends NthRootUnit<{ a: 4; b: -2 }, 2> ? true : never;
+// NthRootUnit
+
+type SquareRootingWorks = IsSame<{ a: 2; b: -1 }, NthRootUnit<{ a: 4; b: -2 }, 2>>;
 const SquareRootingWorks: SquareRootingWorks = true;
 
-type CubeRootingWorks = { a: 1; b: -1 } extends NthRootUnit<{ a: 3; b: -3 }, 3> ? true : never;
+type CubeRootingWorks = IsSame<{ a: 1; b: -1 }, NthRootUnit<{ a: 3; b: -3 }, 3>>;
 const CubeRootingWorks: CubeRootingWorks = true;
+
+// NthRootableUnit
 
 type NthRootableUnitAcceptsPerfectSquares = { a: 2; b: -4; c: 0 } extends NthRootableUnit<2> ? true : never;
 const NthRootableUnitAcceptsPerfectSquares: NthRootableUnitAcceptsPerfectSquares = true;
