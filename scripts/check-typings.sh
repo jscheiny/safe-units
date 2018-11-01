0fe5ce480d6fd3105c5a6cb30b8636db5974c381
Jonah Scheinerman <jonah@scheinerman.net>
2018-11-01 14:20:27 -0400

    Fix typings

--- scripts/check-typings.sh
+++ scripts/check-typings.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-pwd
-
 if grep -q 'import(".*")\.' dist/**/*.d.ts; then 
     echo "ERROR: Found import() typings in dist .d.ts files. Check for exported values with inferred types."
     exit 1
--- src/unit/base.ts
+++ src/unit/base.ts
@@ -1,6 +1,8 @@
+import { GenericMeasure } from "../measure/genericMeasure";
 import { Measure } from "../measure/measure";
 
-export const meters = Measure.dimension("length", "m");
+// HACKHACK: Explicitly type this so we can import GenericMeasure and avoid absolute paths in the generated typings.
+export const meters: GenericMeasure<{ length: 1 }, number> = Measure.dimension("length", "m");
 export const kilograms = Measure.dimension("mass", "kg");
 export const grams = Measure.of(0.001, kilograms, "g");
 export const seconds = Measure.dimension("time", "s");
