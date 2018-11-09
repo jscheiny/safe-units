66eea7f26711445e62509b7052b69a284428308a
Jonah Scheinerman <jonah@scheinerman.net>
2018-11-09 17:38:34 -0500

    Add memory units (#82)

--- src/unit/index.ts
+++ src/unit/index.ts
@@ -4,6 +4,7 @@ import * as US from "./uscu";
 export * from "./angle";
 export * from "./base";
 export * from "./common";
+export * from "./memory";
 export * from "./metric";
 export * from "./other";
 export { Imperial, US };
---
+++ src/unit/memory.ts
@@ -0,0 +1,17 @@
+import { GenericMeasure } from "../measure/genericMeasure";
+import { PrefixFunction } from "../measure/genericMeasureFactory";
+import { Measure } from "../measure/measure";
+
+// HACKHACK: Explicitly type this so we can import GenericMeasure and avoid absolute paths in the generated typings.
+export const bits: GenericMeasure<number, { memory: 1 }> = Measure.dimension("memory", "b");
+export const bytes = Measure.of(8, bits, "B");
+
+// HACKHACK: Explicitly type this so we can import PrefixFunction and avoid absolute paths in the generated typings.
+export const kibi: PrefixFunction<number> = Measure.prefix("Ki", 1 << 10);
+export const mebi = Measure.prefix("Mi", 1 << 20);
+export const gibi = Measure.prefix("Gi", 1 << 30);
+export const tebi = Measure.prefix("Ti", 1 << 40);
+export const pibi = Measure.prefix("Pi", 1 << 50);
+export const exbi = Measure.prefix("Ei", 1 << 60);
+export const zebi = Measure.prefix("Zi", 1 << 70);
+export const yobi = Measure.prefix("Yi", 1 << 80);
