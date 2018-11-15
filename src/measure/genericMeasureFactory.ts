d66999fbb8a8c275c36065fd19c3843394d9ff6c
Jonah Scheinerman <jonah@scheinerman.net>
2018-11-14 19:30:05 -0500

    Extract generic measure internal class

--- src/measure/genericMeasureFactory.ts
+++ src/measure/genericMeasureFactory.ts
@@ -1,18 +1,8 @@
-import { Exponent } from "../exponent";
-import { formatUnit } from "./format";
 import { GenericMeasure, Numeric } from "./genericMeasure";
+import { createMeasureClass } from "./genericMeasureClass";
 import { GenericMeasureStatic, getGenericMeasureStaticMethods } from "./genericMeasureStatic";
-import {
-    BaseUnit,
-    DivideUnits,
-    DivisorUnit,
-    ExponentiateUnit,
-    MultiplicandUnit,
-    MultiplyUnits,
-    Unit,
-    UnitWithSymbols,
-} from "./unitTypeArithmetic";
-import { dimension, divideUnits, exponentiateUnit, multiplyUnits } from "./unitValueArithmetic";
+import { Unit } from "./unitTypeArithmetic";
+import { dimension } from "./unitValueArithmetic";
 
 /** The functions needed to construct a measure of a given numeric type */
 interface GenericMeasureFactory<N> {
@@ -44,7 +34,7 @@ interface GenericMeasureFactory<N> {
     of<U extends Unit>(value: N, quantity: GenericMeasure<N, U>, symbol?: string): GenericMeasure<N, U>;
 }
 
-export type GenericMeasureClass<N, StaticMethods extends {}> = GenericMeasureFactory<N> &
+export type GenericMeasureType<N, StaticMethods extends {}> = GenericMeasureFactory<N> &
     GenericMeasureStatic<N> &
     StaticMethods;
 
@@ -57,152 +47,20 @@ export type GenericMeasureClass<N, StaticMethods extends {}> = GenericMeasureFac
  * type MyMeasure<U extends Unit> = GenericMeasure<U, MyNumberType>;
  * const MyMeasure = createMeasureType({ ... });
  */
-export function createMeasureType<N, S extends {} = {}>(num: Numeric<N>, staticMethods: S): GenericMeasureClass<N, S> {
-    class InternalMeasure<U extends Unit> implements GenericMeasure<N, U> {
-        public readonly symbol: string | undefined;
-        public squared!: U extends BaseUnit<2> ? () => GenericMeasure<N, ExponentiateUnit<U, 2>> : never;
-        public cubed!: U extends BaseUnit<3> ? () => GenericMeasure<N, ExponentiateUnit<U, 3>> : never;
-
-        constructor(public readonly value: N, public readonly unit: UnitWithSymbols<U>, symbol?: string | undefined) {
-            this.symbol = symbol;
-        }
-
-        public withSymbol(symbol: string | undefined): GenericMeasure<N, U> {
-            return new InternalMeasure(this.value, this.unit, symbol);
-        }
-
-        // Arithmetic
-
-        public plus(other: GenericMeasure<N, U>): GenericMeasure<N, U> {
-            return new InternalMeasure(num.add(this.value, other.value), this.unit);
-        }
-
-        public minus(other: GenericMeasure<N, U>): GenericMeasure<N, U> {
-            return new InternalMeasure(num.sub(this.value, other.value), this.unit);
-        }
-
-        public negate(): GenericMeasure<N, U> {
-            return new InternalMeasure(num.neg(this.value), this.unit);
-        }
-
-        public scale(value: N): GenericMeasure<N, U> {
-            return new InternalMeasure(num.mult(this.value, value), this.unit);
-        }
-
-        public times<V extends MultiplicandUnit<U>>(
-            other: GenericMeasure<N, V>,
-        ): GenericMeasure<N, MultiplyUnits<U, V>> {
-            return new InternalMeasure(num.mult(this.value, other.value), multiplyUnits(this.unit, other.unit));
-        }
-
-        public over<V extends DivisorUnit<U>>(other: GenericMeasure<N, V>): GenericMeasure<N, DivideUnits<U, V>> {
-            return new InternalMeasure(num.div(this.value, other.value), divideUnits(this.unit, other.unit));
-        }
-
-        public per<V extends DivisorUnit<U>>(other: GenericMeasure<N, V>): GenericMeasure<N, DivideUnits<U, V>> {
-            return this.over(other);
-        }
-
-        public div<V extends DivisorUnit<U>>(other: GenericMeasure<N, V>): GenericMeasure<N, DivideUnits<U, V>> {
-            return this.over(other);
-        }
-
-        public pow<E extends Exponent>(
-            power: E,
-        ): U extends BaseUnit<E> ? GenericMeasure<N, ExponentiateUnit<U, E>> : never {
-            return new InternalMeasure(num.pow(this.value, power), exponentiateUnit(this.unit as any, power)) as any;
-        }
-
-        public toThe<E extends Exponent>(
-            power: E,
-        ): U extends BaseUnit<E> ? GenericMeasure<N, ExponentiateUnit<U, E>> : never {
-            return this.pow(power);
-        }
-
-        public inverse(): GenericMeasure<N, ExponentiateUnit<U, -1>> {
-            return this.pow(-1);
-        }
-
-        public reciprocal(): GenericMeasure<N, ExponentiateUnit<U, -1>> {
-            return this.pow(-1);
-        }
-
-        public unsafeMap<V extends Unit>(
-            valueMap: (value: N) => N,
-            unitMap?: (unit: UnitWithSymbols<U>) => UnitWithSymbols<V>,
-        ): GenericMeasure<N, U | V> {
-            const newUnit = unitMap === undefined ? this.unit : unitMap(this.unit);
-            return new InternalMeasure<U | V>(valueMap(this.value), newUnit);
-        }
-
-        // Comparisons
-
-        public compare(other: GenericMeasure<N, U>): number {
-            return num.compare(this.value, other.value);
-        }
-
-        public lt(other: GenericMeasure<N, U>): boolean {
-            return this.compare(other) < 0;
-        }
-
-        public lte(other: GenericMeasure<N, U>): boolean {
-            return this.compare(other) <= 0;
-        }
-
-        public eq(other: GenericMeasure<N, U>): boolean {
-            return this.compare(other) === 0;
-        }
-
-        public neq(other: GenericMeasure<N, U>): boolean {
-            return this.compare(other) !== 0;
-        }
-
-        public gte(other: GenericMeasure<N, U>): boolean {
-            return this.compare(other) >= 0;
-        }
-
-        public gt(other: GenericMeasure<N, U>): boolean {
-            return this.compare(other) > 0;
-        }
-
-        // Formatting
-
-        public toString(): string {
-            return `${num.format(this.value)}${formatUnit(this.unit)}`;
-        }
-
-        public in(unit: GenericMeasure<N, U>): string {
-            if (unit.symbol === undefined) {
-                return this.toString();
-            }
-            const value = num.format(num.div(this.value, unit.value));
-            return `${value} ${unit.symbol}`;
-        }
-
-        public clone(): GenericMeasure<N, U> {
-            return new InternalMeasure(this.value, this.unit);
-        }
-    }
-
-    InternalMeasure.prototype.squared = function(): any {
-        return this.toThe(2);
-    };
-
-    InternalMeasure.prototype.cubed = function(): any {
-        return this.toThe(3);
-    };
+export function createMeasureType<N, S extends {} = {}>(num: Numeric<N>, staticMethods: S): GenericMeasureType<N, S> {
+    const Measure = createMeasureClass(num);
 
     const type: GenericMeasureFactory<N> & GenericMeasureStatic<N> = {
         ...getGenericMeasureStaticMethods(),
-        isMeasure: (value): value is GenericMeasure<N, any> => value instanceof InternalMeasure,
+        isMeasure: (value): value is GenericMeasure<N, any> => value instanceof Measure,
         dimension: <Dim extends string>(dim: Dim, symbol?: string): GenericMeasure<N, { [D in Dim]: 1 }> => {
-            return new InternalMeasure(num.one(), dimension(dim, symbol), symbol);
+            return new Measure(num.one(), dimension(dim, symbol), symbol);
         },
         dimensionless: (value: N): GenericMeasure<N, {}> => {
-            return new InternalMeasure(value, {});
+            return new Measure(value, {});
         },
         of: <U extends Unit>(value: N, quantity: GenericMeasure<N, U>, symbol?: string): GenericMeasure<N, U> => {
-            return new InternalMeasure(num.mult(value, quantity.value), quantity.unit, symbol);
+            return new Measure(num.mult(value, quantity.value), quantity.unit, symbol);
         },
     };
 
