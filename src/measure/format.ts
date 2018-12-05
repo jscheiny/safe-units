235a1784429c0414f9ff6d788d678217d7e6ec22
Jonah Scheinerman <jonah@scheinerman.net>
2018-12-05 11:04:11 -0500

    Better unit formatting

--- src/measure/format.ts
+++ src/measure/format.ts
@@ -1,12 +1,30 @@
+import { Exponent } from "../exponent";
 import { SymbolAndExponent, UnitWithSymbols } from "./unitTypeArithmetic";
 
 export function formatUnit(unit: UnitWithSymbols): string {
-    const formatted = Object.keys(unit)
+    const dimensions = Object.keys(unit)
         .map(dimension => unit[dimension])
         .filter(isDimensionPresent)
-        .sort(orderDimensions)
-        .map(formatDimension);
-    return formatted.length === 0 ? "" : " " + formatted.join(" * ");
+        .sort(orderDimensions);
+
+    if (dimensions.length === 0) {
+        return "";
+    }
+
+    const positive = dimensions.filter(([_, dim]) => dim > 0);
+    const negative = dimensions.filter(([_, dim]) => dim < 0);
+
+    if (positive.length === 0) {
+        return formatDimensions(negative);
+    }
+
+    const numerator = formatDimensions(positive);
+    if (negative.length === 0) {
+        return numerator;
+    }
+
+    const denominator = formatDimensions(negative.map(negateDimension));
+    return `${numerator} / ${maybeParenthesize(denominator, negative.length !== 1)}`;
 }
 
 function isDimensionPresent(dimension: SymbolAndExponent | undefined): dimension is SymbolAndExponent {
@@ -17,7 +35,19 @@ function orderDimensions([leftSymbol]: SymbolAndExponent, [rightSymbol]: SymbolA
     return leftSymbol < rightSymbol ? -1 : 1;
 }
 
-function formatDimension([symbol, exponent]: SymbolAndExponent): string {
-    const exponentStr = exponent !== 1 ? `^${exponent}` : "";
-    return `${symbol}${exponentStr}`;
+function formatDimensions(dimensions: SymbolAndExponent[]): string {
+    return dimensions
+        .map(([symbol, exponent]) => {
+            const exponentStr = exponent !== 1 ? `^${exponent}` : "";
+            return `${symbol}${exponentStr}`;
+        })
+        .join(" * ");
+}
+
+function negateDimension([symbol, exponent]: SymbolAndExponent): SymbolAndExponent {
+    return [symbol, -exponent as Exponent];
+}
+
+function maybeParenthesize(text: string, parenthesize: boolean): string {
+    return parenthesize ? `(${text})` : text;
 }
