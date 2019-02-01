dcbdf3510cc27c9735e4f591a46da5bdc977c30d
Jonah Scheinerman <jonah@scheinerman.net>
2019-02-01 15:37:47 -0500

    Simplify IsSame type

--- test/types/utils.ts
+++ test/types/utils.ts
@@ -1,2 +1,1 @@
-export type IsSame<A, B> = IsSameImpl<{ t: A }, { t: B }>;
-type IsSameImpl<A, B> = A extends B ? (B extends A ? true : false) : false;
+type IsSame<A, B> = [A] extends [B] ? ([B] extends [A] ? true : false) : false;
