fb2d9b2e0625e2bcf11a3bcf3c6188e9d16804f2
Jonah Scheinerman <jonah@scheinerman.net>
2019-07-07 14:00:57 -0400

    Fix lint

--- test/types/measures.ts
+++ test/types/measures.ts
@@ -2,30 +2,30 @@ import { Acceleration, Area, kilograms, Measure, meters, newtons, seconds, Volum
 
 // Valid usages
 
-Measure.dimension("x"); // $ExpectType IGenericMeasure<number, { x: "1"; }>
+Measure.dimension("x"); // $ExpectType GenericMeasure<number, { x: "1"; }>
 
-const m = meters; // $ExpectType IGenericMeasure<number, { length: "1"; }>
-const s = seconds; // $ExpectType IGenericMeasure<number, { time: "1"; }>
-const n = newtons; // $ExpectType IGenericMeasure<number, { length: "1"; mass: "1"; time: "-2"; }>
+const m = meters; // $ExpectType GenericMeasure<number, { length: "1"; }>
+const s = seconds; // $ExpectType GenericMeasure<number, { time: "1"; }>
+const n = newtons; // $ExpectType GenericMeasure<number, { length: "1"; mass: "1"; time: "-2"; }>
 
-const a = newtons.over(kilograms); // $ExpectType IGenericMeasure<number, { length: "1"; time: "-2"; }>
-const accel: Acceleration = a; // $ExpectType IGenericMeasure<number, { length: "1"; time: "-2"; }>
+const a = newtons.over(kilograms); // $ExpectType GenericMeasure<number, { length: "1"; time: "-2"; }>
+const accel: Acceleration = a; // $ExpectType GenericMeasure<number, { length: "1"; time: "-2"; }>
 
-const absement = meters.times(seconds); // $ExpectType IGenericMeasure<number, { length: "1"; time: "1"; }>
-const velocity = meters.over(seconds); // $ExpectType IGenericMeasure<number, { length: "1"; time: "-1"; }>
+const absement = meters.times(seconds); // $ExpectType GenericMeasure<number, { length: "1"; time: "1"; }>
+const velocity = meters.over(seconds); // $ExpectType GenericMeasure<number, { length: "1"; time: "-1"; }>
 
-meters.plus(meters); // $ExpectType IGenericMeasure<number, { length: "1"; }>
-meters.minus(meters); // $ExpectType IGenericMeasure<number, { length: "1"; }>
-meters.negate(); // $ExpectType IGenericMeasure<number, { length: "1"; }>
-meters.scale(2); // $ExpectType IGenericMeasure<number, { length: "1"; }>
+meters.plus(meters); // $ExpectType GenericMeasure<number, { length: "1"; }>
+meters.minus(meters); // $ExpectType GenericMeasure<number, { length: "1"; }>
+meters.negate(); // $ExpectType GenericMeasure<number, { length: "1"; }>
+meters.scale(2); // $ExpectType GenericMeasure<number, { length: "1"; }>
 
-velocity.squared(); // $ExpectType IGenericMeasure<number, { length: "2"; time: "-2"; }>
-absement.cubed(); // $ExpectType IGenericMeasure<number, { length: "3"; time: "3"; }>
-absement.inverse(); // $ExpectType IGenericMeasure<number, { length: "-1"; time: "-1"; }>
-velocity.toThe("0"); // $ExpectType IGenericMeasure<number, {}>
+velocity.squared(); // $ExpectType GenericMeasure<number, { length: "2"; time: "-2"; }>
+absement.cubed(); // $ExpectType GenericMeasure<number, { length: "3"; time: "3"; }>
+absement.inverse(); // $ExpectType GenericMeasure<number, { length: "-1"; time: "-1"; }>
+velocity.toThe("0"); // $ExpectType GenericMeasure<number, {}>
 
-Measure.sqrt(velocity.toThe("-4")); // $ExpectType IGenericMeasure<number, { length: "-2"; time: "2"; }>
-Measure.cbrt(absement.toThe("3")); // $ExpectType IGenericMeasure<number, { length: "1"; time: "1"; }>
+Measure.sqrt(velocity.toThe("-4")); // $ExpectType GenericMeasure<number, { length: "-2"; time: "2"; }>
+Measure.cbrt(absement.toThe("3")); // $ExpectType GenericMeasure<number, { length: "1"; time: "1"; }>
 
 // Error usages
 
